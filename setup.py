f6979de2bdcb86240d86650faec78567c4f71766
kylejamesbyron <markandrewisom@gmail.com>
2024-04-14 14:33:45 -0400

    Got Narratives working

--- setup.py
+++ setup.py
@@ -17,10 +17,7 @@ app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 # End of opening
 
 # Settings List
-settings = [('narrative1,',),
-            ('narrative2,',), 
-            ('narrative3,',),
-            ('narrative4,',),
+settings = [('narrative',),
             ('projectname',),
             ('database',),
             ('logline',),]
@@ -39,10 +36,12 @@ def createdb():
    open(database, 'x')
    connection = sqlite3.connect(database)
    cursor = connection.cursor()
-   cursor.execute("CREATE TABLE settings (KEY INTEGER UNIQUE, VARIABLE TEXT, SETTING TEXT,\
-                   PRIMARY KEY('KEY' AUTOINCREMENT))")
+   cursor.execute("CREATE TABLE settings (KEY INTEGER UNIQUE, VARIABLE TEXT UNIQUE,\
+                   SETTING TEXT, NNUMBER INTEGER, PRIMARY KEY('KEY' AUTOINCREMENT))")
    connection.commit()
    cursor.executemany("INSERT INTO settings (VARIABLE) VALUES (?)", (settings))
+   connection.commit()
+   cursor.execute("UPDATE settings SET NNUMBER = ? WHERE VARIABLE == ?", (0, 'narrative'))
    connection.commit()
    
    site = '/newproject/' + project_name
@@ -52,7 +51,8 @@ def createdb():
 def newproject(project_name):
    projectname = project_name.replace("_", " ")
    database = 'projdbs/' + project_name + '.db'
-   return render_template('newproject.html', projectname=projectname, project_name=project_name,\
+   return render_template('newproject.html',\
+                           projectname=projectname, project_name=project_name,\
                           database=database)
 
 @app.route('/savesettings', methods=['post'])
@@ -60,7 +60,6 @@ def savesettings():
    projectname = request.form['projectname']
    database = request.form['database']
    logline = request.form['logline']
-   #narrative1 = request.form['narrative1']
    connection = sqlite3.connect(database)
    connection.row_factory = sqlite3.Row
    cursor = connection.cursor()
@@ -84,14 +83,41 @@ def savesettings():
          connection.commit()
 
     
-   cursor.execute("SELECT VARIABLE FROM settings WHERE VARIABLE LIKE ?", ('narrative%',)) 
+   cursor.execute("SELECT * FROM settings WHERE KEY is not NULL") 
    narratives = cursor.fetchall() 
    for narrative in narratives:
-      narrative = narrative['variable']
+      if narrative['VARIABLE'].startswith('narrative'):
+         NNUMBER = narrative['NNUMBER']
+         nnumberstr = str(NNUMBER)
 
 
-   return render_template('/createnarratives.html', projectname=projectname, logline=logline, database=database)
-   #return narrative
+   return render_template('newnarratives.html', projectname=projectname,\
+    logline=logline, database=database, narratives=narratives, nnumberstr=nnumberstr, NNUMBER=NNUMBER)
+
+
+# Save Narrative
+@app.route('/savenarrative', methods=['post'])
+def savenarrative():
+   projectname = request.form['projectname']
+   database = request.form['database']
+   logline = request.form['logline']
+   newnarrative = request.form['newnarrative']
+   newnarrativenumber = request.form['newnarrativenumber']
+   newnarrativename = 'narrative' + str(newnarrativenumber)
+   connection = sqlite3.connect(database)
+   connection.row_factory = sqlite3.Row
+   cursor = connection.cursor()
+   cursor.execute("INSERT INTO settings (VARIABLE, SETTING, NNUMBER) VALUES (?, ?, ?)", (newnarrativename, newnarrative, newnarrativenumber))
+   connection.commit()
+   cursor.execute("SELECT * FROM settings WHERE KEY is not NULL") 
+   narratives = cursor.fetchall() 
+   for narrative in narratives:
+      if narrative['VARIABLE'].startswith('narrative'):
+         NNUMBER = narrative['NNUMBER']
+         nnumberstr = str(NNUMBER)
+   return render_template('createnarratives.html', projectname=projectname,\
+                          logline=logline, database=database, narratives=narratives,\
+                             nnumberstr=nnumberstr, NNUMBER=NNUMBER)
 
 
 # Close Flask
